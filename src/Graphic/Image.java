0fcfacb08cefe5c7649a3cb3724fb8376554f168
AnUnluckyGuy <53502159+AnUnluckyGuy@users.noreply.github.com>
2022-06-25 13:45:37 +0700

    add NPC image

--- src/Graphic/Image.java
+++ src/Graphic/Image.java
@@ -13,6 +13,8 @@ public class Image {
 
     public static final Image PLAYER = new Image("res/Sprite/player.png");
     public static final Image OPPONENT = new Image("res/Sprite/opponent.png");
+    public static final Image OPPONENT = new Image("res/Sprite/NPC.png");
+
 
     public Image(int imageCol, int imageRow, String path) {
         this.imageCol = imageCol;
